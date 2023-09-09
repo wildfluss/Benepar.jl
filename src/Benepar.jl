0ce8e9dcf0914546ab1456b64d26428dc725c10b
Yakov Wildfluss <yakov@wildfluss.com>
2023-09-09 14:56:11 +0300

    try create my first julia pkg

---
+++ src/Benepar.jl
@@ -0,0 +1,25 @@
+module Benepar
+
+using PythonCall
+
+export benepar
+
+function __init__()
+    global benepar = pyimport("benepar")
+end
+
+# function __init__()
+#     "spacy" in Conda._installed_packages() || Conda.add("spacy");
+#     global spacy = pyimport("spacy")
+# end
+
+# const python3 = joinpath(Conda.python_dir(Conda.ROOTENV), "python3")
+
+# function load(model::String = "en_core_web_sm")
+#     spacy.util.is_package(model) ?
+#         @info("Model `$model` is already installed") :
+#         run(`$python3 -m spacy download $model`)
+#     spacy.load(model)
+# end
+
+end
